291c81a2e71bcee651164a63335aa57f5a45d5bc
roosikem <manishprajapati92@gmail.com>
2018-05-06 22:56:00 +0530

    Update connection.php

--- lostpeople/php/connection.php
+++ lostpeople/php/connection.php
@@ -1,11 +1,12 @@
 <?php
-    
-    $dbhost = 'localhost';
-    $dbuser = 'androidl_anupama';
-    $dbpass = 'MNAnupama@112';
+
+    $dbname = getenv("dbname");
+    $dbhost = getenv("MYSQL_SERVICE_HOST");
+    $dbuser =  getenv("dbusername");
+    $dbpass = getenv("dbpassword");
     $conn = mysql_connect($dbhost, $dbuser, $dbpass);    
     if(!$conn) {
         die('Could not connect: ' . mysql_error());
     }
-    mysql_select_db('androidl_labmanager');
+    mysql_select_db($dbname);
 ?>
